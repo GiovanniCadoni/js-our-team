2e7d7802d774400e3a3ddbe6e60a5d66e39db779
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-06 19:53:05 +0100

    add: array that contain every employee and his info

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const employee = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "../img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "../img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "../img/walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "../img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "../img/scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "../img/barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+console.log(employee);
